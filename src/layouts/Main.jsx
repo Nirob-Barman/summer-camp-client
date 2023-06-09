0b6bb021762d6b4447212a9b6c8cdd95cb8ad901
Nirob Barman <niroborin111@gmail.com>
2023-06-09 15:06:24 +0600

    Login Page

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,20 +1,22 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./pages/Shared/Footer/Footer"
 import NavBar from "./pages/Shared/NavBar/NavBar"
-import SummerCampSection from "./pages/Home/Home/SummerCampSection/SummerCampSection"
+// import SummerCampSection from "./pages/Home/Home/SummerCampSection/SummerCampSection"
 
 const Main = () => {
   return (
     <div>
       <NavBar />
 
-      <div className="pt-20">
+      {/* <div className="pt-20">
         <SummerCampSection />
-      </div>
+      </div> */}
 
       <h1
       // className="text-center pt-20"
-      >Welcome TO Summer Camp School</h1>
+      >
+        {/* Welcome TO Summer Camp School */}
+      </h1>
 
       <Outlet />
       <Footer />
--- src/layouts/pages/Home/Home/Home.jsx
+++ src/layouts/pages/Home/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import SummerCampSection from '../../Home/Home/SummerCampSection/SummerCampSection';
 
 const Home = () => {
     return (
         <div>
+            <div className="pt-20">
+                <SummerCampSection />
+            </div>
             <Banner />
+
             category
             class
             section
